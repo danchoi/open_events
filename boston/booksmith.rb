55992250bcae8ddca1b6e76f6bd14ed020e30b44
Daniel Choi <dhchoi@gmail.com>
2011-06-20 12:17:57 -0400

    Improve DSL

--- boston/booksmith.rb
+++ boston/booksmith.rb
@@ -1,39 +1,30 @@
-require 'event_scraper'
+info title: 'Brookline Booksmith',
+     url: 'http://www.brooklinebooksmith.com/events/mainevent.html',
+     categories: %w(books speakers),
+     locations: %w(coolidge-corner)
 
-class Booksmith < EventScraper
 
-  def about
-    { 
-      title: 'Brookline Booksmith',
-      url: 'http://www.brooklinebooksmith.com/events/mainevent.html',
-      categories: %w(books speakers),
-      locations: %w(coolidge-corner)
-    }
-  end
+nodes {
+  doc.search('strong a').
+  select {|x| 
+    x['href'] =~ %r{^http://www.brooklinebooksmith-shop.com/event/}}.
+  map {|x| 
+    x.ancestors.detect {|y| y.name == 'tr'}}
+}
 
-  def nodes
-    doc.search('strong a').
-      select {|x| 
-        x['href'] =~ %r{^http://www.brooklinebooksmith-shop.com/event/}}.
-      map {|x| 
-        x.ancestors.detect {|y| y.name == 'tr'}}
-  end
-
-  def event(n)
-    return n.inner_html
-    date = if (x = n.at('.entry-meta'))
-             x.inner_text
-           else
-             @res.last && @res.last[:date]
-           end
-    time = (x = n.at('li/text()')) && x.text.strip
-    link = n.at('.entry-title a')['href']
-    { 
-      date: date,
-      time: time,
-      title: n.at('.entry-title').inner_text,
-      link: link 
-    }
-  end
-end
-
+event {|n| 
+  return n.inner_html
+  date = if (x = n.at('.entry-meta'))
+           x.inner_text
+         else
+           @res.last && @res.last[:date]
+         end
+  time = (x = n.at('li/text()')) && x.text.strip
+  link = n.at('.entry-title a')['href']
+  { 
+    date: date,
+    time: time,
+    title: n.at('.entry-title').inner_text,
+    link: link 
+  }
+}
--- lib/event_scraper.rb
+++ lib/event_scraper.rb
@@ -1,34 +1,63 @@
-require 'cached_html'
 require 'nokogiri'
+require 'open-uri'
 
 class EventScraper
-  include CachedHtml
   attr_accessor :doc
 
-  def initialize
-    @doc = Nokogiri::HTML.parse(self.html)
+  def initialize(path)
+    code = File.read(path)
+    instance_eval code
+  end
+
+  def info(values={})
+    @info ||= values
+  end
+
+  def nodes(&block)
+    @node_finder ||= block
+  end
+
+  def event(&block)
+    @event_parser ||= block
   end
 
   def html
-    @html ||= cached_html(about[:url])
+    @html ||= open(info[:url]).read
+  end
+
+  def doc
+    @doc ||= Nokogiri::HTML.parse(html)
+  end
+
+  def parsed_nodes
+    @parsed_nodes ||= nodes.call
+  end
+
+  def parsed_event(n)
+    event.call n
   end
 
   def parse
+    @doc = Nokogiri::HTML.parse(self.html)
     @res = []
-    nodes.map {|n|
-      @res << event(n)
+    parsed_nodes.map {|n|
+      @res << parsed_event(n)
     }
-    
     @res.delete_if { |x| x.nil? }
   end
 
   def parse_test
-    nodes.each {|n|
+    parsed_nodes.each {|n|
       puts n.inner_html
       puts n.inner_text.gsub(/\s{2,}/, ' ').strip
       puts '-' * 80
     }
   end
+end
 
+if __FILE__ == $0
+  es = EventScraper.new ARGV.first
+  puts es.info
+  es.parse_test
 end
 
